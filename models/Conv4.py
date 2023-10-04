b6defa8901cecfff1e2734b5dd9569df4dabea05
Zixuan Zhu <44810777+Zixuan-Zhu@users.noreply.github.com>
2023-10-04 10:49:07 +0800

    Add files via upload

---
+++ models/Conv4.py
@@ -0,0 +1,30 @@
+import torch.nn as nn
+
+# Basic ConvNet with Pooling layer
+def conv_block(in_channels, out_channels):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, 3, padding=1),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU(),
+        nn.AvgPool2d(2)
+    )
+
+
+class ConvNet(nn.Module):
+
+    def __init__(self, x_dim=3, num_class=10):
+        super().__init__()
+        self.encoder = nn.Sequential(
+            conv_block(x_dim, 16),
+            conv_block(16, 64),
+            conv_block(64, 64),
+            # conv_block(256, 512),
+        )
+        self.fc = nn.Linear(64, num_class)
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = nn.AdaptiveAvgPool2d(1)(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
